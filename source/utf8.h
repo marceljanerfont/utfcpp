23cd36fe40a7cb2b543f252f24dffca0dfd88a6c
marcel janer font <marceljanerfont@users.noreply.github.com>
2022-02-09 07:03:42 +0100

    added static, to avoid includes troubles and increase performance

--- source/utf8.h
+++ source/utf8.h
@@ -40,7 +40,7 @@ namespace utf8 {
  * \param std::wstring utf16_str encoded in UTF-16 if WINDOWS or UTF-32 if POSIX
  * \return std::string utf8_str encoded in UTF-8
  */
-std::string toUtf8(const std::wstring &utf16_str) {
+inline std::string toUtf8(const std::wstring &utf16_str) {
   std::string utf8_str;
 #ifdef __linux__
   utf8::utf32to8(utf16_str.begin(), utf16_str.end(), back_inserter(utf8_str));
@@ -54,7 +54,7 @@ std::string toUtf8(const std::wstring &utf16_str) {
 * \param std::string utf8_str encoded in UTF-8
 * \return std::wstring utf16_str encoded in UTF-16 if WINDOWS or UTF-32 if POSIX
 */
-std::wstring fromUtf8(const std::string &utf8_str) {
+inline std::wstring fromUtf8(const std::string &utf8_str) {
   std::wstring utf16_str;
 #ifdef __linux__
   utf8::utf8to32(utf8_str.begin(), utf8_str.end(), back_inserter(utf16_str));
